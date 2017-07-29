08be62451de4a6a2e595802d74cb6319606389d2
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2017-07-29 21:47:44 +0200

    Initial commit

---
+++ contracts/Greetings.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+
+contract Greetings {
+     string message;
+
+     function Greetings() {
+          message = "I am ready!";
+     }
+
+     function setGreetings(string _message) public {
+          message = _message;
+     }
+
+     function getGreetings() constant returns (string) {
+          return message;
+     }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Greetings = artifacts.require("./Greetings.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Greetings);
+};
